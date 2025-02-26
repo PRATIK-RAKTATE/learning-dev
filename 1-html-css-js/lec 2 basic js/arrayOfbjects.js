287bd6c5accaf792316e6231f4aef5b509ee923f
Pratik Raktate <rhutikraktate@gmail.com>
2025-02-26 20:30:29 +0530

    Change file name

---
+++ 1-html-css-js/lec 2 basic js/arrayOfbjects.js
@@ -0,0 +1,18 @@
+
+// In j we can nest array and objects as we want
+const objOfobj = [
+     a = "pratik",
+
+     b = {
+        k : "rhutik"
+     }
+    ]
+
+const c = {
+    c : "rhutik"
+}
+
+// at first index of array and object c is same
+
+console.log(c);
+console.log(objOfobj[1]);
